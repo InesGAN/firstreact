1fc8cbc0ec155aefffc572bd5711b264976b3183
ines G <inesgannoun2@gmail.com>
2020-10-20 17:33:16 +0200

    first react app

--- src/App.js
+++ src/App.js
@@ -6,18 +6,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="formulaire">
+ <form class="text-center border border-light p-5" action="#!">
+
+    <p class="h4 mb-4">Sign in</p>
+
+    <input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail"/>
+
+
+    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password"/>
+
+    <div class="d-flex justify-content-around">
+        <div>
+      
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember"/>
+                <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+            </div>
+        </div>
+        <div>
+     
+            <a href="">Forgot password?</a>
+        </div>
+    </div>
+
+
+    <button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+
+
+    <p>Not a member?
+        <a href="">Register</a>
+    </p>
+
+    <p>or sign in with:</p>
+
+    <a href="#" class="mx-2" role="button"><i class="fa fa-facebook-f light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fa fa-twitter light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fa fa-linkedin-in light-blue-text"></i></a>
+    <a href="#" class="mx-2" role="button"><i class="fa fa-github light-blue-text"></i></a>
+
+</form>
+</div>
       </header>
     </div>
   );
